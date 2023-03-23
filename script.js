556895420cfabc760e9b55c75242d3ad8e486e96
123Tamilmani <cecstamilmaniv24@gmail.com>
2023-03-23 13:37:42 +0530

    23.3.23 1:30

---
+++ script.js
@@ -0,0 +1,6 @@
+// const homebtn = document.querySelector('.onsubmithome');
+// // homebtn.classList.remove('hidden');
+// const home = addEventListener(onclick,'.onsubmithome')
+// {
+//     homebtn.classList.add('hidden');
+// }
